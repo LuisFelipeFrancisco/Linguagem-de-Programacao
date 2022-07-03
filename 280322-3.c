2dc9a9f5be99f0c742259df925d99d99e5957f4c
Trunkios <luisfelipe_ferreira@hotmail.com>
2022-07-03 13:27:34 -0300

    Create 280322-3.c
    Escrever uma função que receba como parâmetros de entrada as dimensões de um retângulo e retorne o valor da area correspondente

---
+++ 280322-3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+float mult (int x, int y) // definição da função
+{
+	float result;
+	result = x*y;
+	return result;		// retorno da função
+}
+main (){
+
+double n1,n2, area;
+
+printf("Digite a largura do retangulo: ");
+scanf ("%f", &n1);
+printf("Digite a altura do retangulo: ");
+scanf ("%f", &n2);
+
+area = mult (n1, n2); // chamado da função
+printf ("Area do retangulo: %lf\n", area);  	
+
+
+	
+}
