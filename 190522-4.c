4fb4d3a352b379283c3fc48cec8e572061b784c6
Trunkios <luisfelipe_ferreira@hotmail.com>
2022-07-03 14:35:28 -0300

    Create 190522-4.c
    Serie harmônica 
    S = 2 + 5/2 + 10/3 + 16/4 ...

---
+++ 190522-4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+float calculaS (int n)
+{
+	if (n==1)
+		return 1;
+	else return (float) (1+(n*n))/n+calculaS(n-1);
+	
+}
+
+main ()
+{
+	int num;
+	do 
+	{
+		printf("Numero: \n");
+		scanf ("%d", &num);
+	}
+while (num<1);
+
+printf ("Serie S: %.3f",calculaS(num));
+	
+}
