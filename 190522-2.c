0fea75eed87fb9b907be4ae01e99093de5ef4d9d
Trunkios <luisfelipe_ferreira@hotmail.com>
2022-07-03 14:33:55 -0300

    Create 190522-2.c
    Escrever um programa (principal e função recursiva), que calcule a combinação (n,p)
    N e o P devem ser inteiros positivos, e P menor que N.
    combinação (n,p) = n! = n!/p!*(n-p)!

---
+++ 190522-2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int calculaCOMB (int n)
+{
+	if (n==1)
+		return 1;
+	else return n*calculaCOMB(n-1);
+}
+
+main (){
+	int n, p;
+	
+	do{
+		printf("Valor do N: ");
+		scanf ("%d", &n);
+	} while (n<1);
+	
+	do {
+		printf("\nValor de P: ");
+		scanf ("%d", &p);
+	} while ((p<1) || (p>=n));
+	
+	printf("\n\nArranjo (%d;%d) = %.2f", n,p,(float) calculaCOMB(n)/ calculaCOMB(p)*calculaCOMB(n-p));
+}
