79e17f41fa012d15d57cdb6b0e23c081f1fadf3a
Luis Felipe Francisco <110519969+LuisFelipeFrancisco@users.noreply.github.com>
2022-08-14 02:57:30 -0300

    Power of a number
    Calculating the power of a number

--- 100422-2.c
+++ 100422-2.c
@@ -1,33 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int potencia (int base, int expoente)
+/* It calculates the power of a number */
+
+int potencia(int base, int expoente)
 {
-	int result, aux;
-		if(expoente == 0)
-		printf ("\n%d elevado a %d = 1\n",base, expoente);
-	else 
-	{
-		result = 1;
-		aux = 0;
-		while (aux != expoente)
-		{
-			result *= base;
-			aux++;
-		}
-		printf ("\n%d elevado a %d = %d\n",base, expoente,result);
-	}		
-return 0;
+/* Checking if the exponent is 0, if it is, it will print the result ("1"). */
+    int result, aux;
+    if (expoente == 0)
+        printf("\n%d elevado a %d = 1\n", base, expoente);
+/* Calculating the power of a number. */
+    else
+    {
+        result = 1;
+        aux = 0;
+/* Checking if the value of `aux` is different from `expoente`. */
+        while (aux != expoente)
+        {
+/* Multiplying the result by the base. */
+            result *= base;
+/* Incrementing the value of `aux` by 1. */
+            aux++;
+        }
+/* Printing the result of the power of a number. */
+        printf("\n%d elevado a %d = %d\n", base, expoente, result);
+    }
+    return 0;
 }
 
-main ()
+main()
 {
-	int a,b, res;
-	printf("Digite a base:\n");
-	scanf("%d", &a);
-	printf ("Digite o expoente:\n");
-	scanf("%d", &b);
-	res = potencia (a,b);
+    int a, b, res;
+    printf("Digite a base:\n");
+    scanf("%d", &a);
+    printf("Digite o expoente:\n");
+    scanf("%d", &b);
+    res = potencia(a, b);
 
-system("pause");	
+    system("pause");
 }
