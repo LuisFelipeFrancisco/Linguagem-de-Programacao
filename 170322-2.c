67de32cf39f4a1015ddc206f432e41f30ce16ca9
Luis Felipe Francisco <110519969+LuisFelipeFrancisco@users.noreply.github.com>
2022-08-14 00:59:37 -0300

    Matrix 4x4 - Secondary diagonal sum
    Reading a 4x4 matrix and printing the sum of the secondary diagonal.

--- 170322-2.c
+++ 170322-2.c
@@ -3,30 +3,30 @@
 
 main ()
 {
-	int i, j, soma, mat[4][4];
-	
-	for (i=0;i<4;i++)
-		for (j=0;j<4;j++)
-		{
-			printf ("Digite o elemento da matriz [%d][%d]: ", i,j);
-			scanf ("%d", &mat[i][j]);
-		}
-	j=3;
-	for(i=0;i<4;i++)
-	{
-		soma=soma+mat[i][j];
-		j--;
-	}
-	
-	printf ("\n\nMatriz Lida: \n");
-	for (i=0;i<4;i++)
-	{
-		
-		for (j=0;j<4;j++)
-		
-			printf("%4d", mat[i][j]);
-			printf("\n");			
-	}
-	printf("\n\nSoma da Diagonal Segundaria: %d", soma);
-	
+    /* Reading a 4x4 matrix and printing the sum of the secondary diagonal. */
+    int i, j, soma, mat[4][4];
+
+    for (i = 0; i < 4; i++)
+        for (j = 0; j < 4; j++)
+        {
+            printf("Digite o elemento da matriz [%d][%d]: ", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+    j = 3;
+    for (i = 0; i < 4; i++)
+    {
+        soma = soma + mat[i][j];
+        j--;
+    }
+
+    printf("\n\nMatriz Lida: \n");
+    for (i = 0; i < 4; i++)
+    {
+
+        for (j = 0; j < 4; j++)
+
+            printf("%4d", mat[i][j]);
+        printf("\n");
+    }
+    printf("\n\nSoma da Diagonal Segundaria: %d", soma);
 }
