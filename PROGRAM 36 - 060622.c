b634c448ee79dcbc9cb83dea2581fd058a2dc366
Luis Felipe Francisco <110519969+LuisFelipeFrancisco@users.noreply.github.com>
2022-08-20 13:54:30 -0300

    Number of characters in the string
    Asking the user to input a string and then it is printing the number of characters in the string.

--- PROGRAM 36 - 060622.c
+++ PROGRAM 36 - 060622.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
-int contadordeletras(char palavra[], int n)
-/* Checking if the character in the position n is the end of the string. If it is, it returns 0. 
-If it is not, it returns 1 plus the number of characters in the string. */
+int contadordeletras(char palavra[], int n) // conta o numero de letras de uma palavra.
 {
-    if (palavra[n] == '\0')
+    if (palavra[n] == '\0') // se a palavra acabar, retorna 0.
         return 0;
     else
-        return 1 + contadordeletras(palavra, n + 1);
+        return 1 + contadordeletras(palavra, n + 1); // se ainda nao acabou, retorna 1 mais o numero de letras da palavra.
 }
 
 main()
 {
-/* Asking the user to input a string and then it is printing the number of characters in the string. */
-    char palavra[100];
+    char palavra[100]; // cria uma variavel para armazenar a palavra.
 
-    printf("Digite uma palavra:\n");
-        gets(palavra);
-    printf("\nNumeros de caracteres da palavra digitada: %d", contadordeletras(palavra, 0));
+    printf("Digite uma palavra:\n"); // pede para o usuario digitar uma palavra.
+        gets(palavra); // armazena a palavra no vetor.
+    printf("\nNumeros de caracteres da palavra digitada: %d", contadordeletras(palavra, 0)); // imprime o numero de letras da palavra.
 }
